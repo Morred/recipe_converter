de605c14e86e582fc23c8890d507331cf3cb97f3
Laura Eck <laura.eck@gmx.net>
2013-06-04 12:06:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeConverter::Application.config.secret_token = 'cda98bb2746759390af574b220d8e4bdf388bfa8b74e6916a9285631e659857618f7a7b0b270e94ae6ce40ec83f5f4145e77bcfe8400c82d856a3c77a3dbba7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RecipeConverter::Application.config.session_store :cookie_store, key: '_recipe_converter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RecipeConverter::Application.config.session_store :active_record_store
