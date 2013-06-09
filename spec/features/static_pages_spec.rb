7369610e52198bb1d502dae855cd5fc8a3aae450
Laura Eck <laura.eck@gmx.net>
2013-06-09 20:34:13 +0200

    Finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,39 +5,57 @@ describe "StaticPages" do
 	describe "Home Page" do
 
 		it "should have the h1 'Recipe Converter'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text => 'Recipe Converter')
 		end
 
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "RecipeConverter | Home")
+		it "should have the base title" do
+			visit root_path
+			page.should have_title("RecipeConverter")
+		end
+
+		it "should not have a custom page title" do
+			visit root_path
+			page.should_not have_title("| Home")
 		end
 	end
 
 	describe "Help page" do
 		
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('title', :text => "RecipeConverter | Help")
+			visit help_path
+			page.should have_title("RecipeConverter | Help")
 		end
 	end
 
 	describe "About page" do
 
 		it "should have the h1 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About Us')
 		end
 
 		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "RecipeConverter | About Us")
+			visit about_path
+			page.should have_title("RecipeConverter | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact'" do
+			visit contact_path
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit contact_path
+			page.should have_title("RecipeConverter | Contact")
 		end
 	end
 end
